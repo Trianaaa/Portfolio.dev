80937b16c6921bb5af105060b55ca6926bb52db4
Pablo Triana <triana@Mac-mini-de-Pablo.local>
2025-07-25 18:40:20 -0500

    Updating information and changing functionality.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+        'grid-light': "radial-gradient(#e5e7eb_1px, transparent_1px)",
+        'grid-dark': "radial-gradient(#27272a_1px, transparent_1px)",
+      },
+    },
+  },
+  plugins: [],
 }
